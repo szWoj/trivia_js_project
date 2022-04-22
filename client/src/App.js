532e54fc156b291965e3c467ade1b073bf6f72fe
Annika Neumann <annika1983neumann@gmail.com>
2022-04-22 18:47:12 +0100

    background image, opacity, text on image

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,13 @@ import Scores from './components/Scores';
 function App() {
   return (
     <>
+      <img className="hero-image" src={require("./images/yellowbrickroad2.jpeg")} alt='Wizard of Oz'/>
+      <div className='hero-text'>
       <h1>The Quizard of Oz</h1>
       <Form />
       <Scores />
       <QuizList />
+      </div>
     </>
   );
 }
