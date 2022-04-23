f6101c63e5e4589d2ea03334f36a51bc977a168e
Annika Neumann <annika1983neumann@gmail.com>
2022-04-23 22:59:24 +0100

    continued styling form.js

--- client/src/components/QuizItem.js
+++ client/src/components/QuizItem.js
@@ -5,7 +5,8 @@ const QuizItem = () => {
     return (
         <>
         <p>quiz question goes here</p>
-        <img className="wizard" src={require("../images/wizard2.jpeg")} alt='Wizard of Oz'/> 
+        {/* <img className="wizard" src={require("../images/wizard2.jpeg")} alt='Wizard of Oz'/>  */}
+
         </>
 
     )
