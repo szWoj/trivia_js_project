37959dc434d055b20c931c93b16c891b9dbeb261
Annika Neumann <annika1983neumann@gmail.com>
2022-04-23 12:54:58 +0100

    edited scores.js

--- client/src/components/Scores.js
+++ client/src/components/Scores.js
@@ -3,8 +3,8 @@ import {getScores} from '../QuizService.js'
 const Scores = () => {
 
     const scores = getScores();
-    scoresDisplay = scores.map(score => {
-        return <p>score[category] : score[result]</p>
+    const scoresDisplay = scores.map(score => {
+        return <p>{score[category]} : {score[result]}</p>
     })
 
     return (
