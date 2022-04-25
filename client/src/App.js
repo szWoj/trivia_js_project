81982b6bc6847bfa3e135d23e0d7a8414c4f66da
Annika Neumann <annika1983neumann@gmail.com>
2022-04-25 09:15:46 +0100

    added flexbox container and div to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -29,9 +29,11 @@ function App() {
       <img className="hero-image" src={require("./images/yellowbrickroad2.jpeg")} alt='Wizard of Oz'/>
       <div className='hero-text'>
       <h1>The Quizard of Oz</h1>
-      <Form getQuestions={getQuestions}/>
-      <Scores />
-      <QuizList questions={questions}/>
+      <div className='flex-container'>
+      <div className='item1'><Form getQuestions={getQuestions}/></div>
+      <div className='item2'><Scores /></div>
+      <div className='item2'><QuizList questions={questions}/></div>
+      </div>
       </div>
       <footer>2022 - Created by Annika, Daniel and Szymon - Smart as fuck</footer>
     </>
